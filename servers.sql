543229ad02a77c32f45ec3efee8f781b82c9dbe3
Thierno Barry <thiernobarry@Thiernos-Mac-mini.local>
2022-12-20 18:46:14 -0500

    table client added and contraints amended

--- servers.sql
+++ servers.sql
@@ -2,9 +2,10 @@
 -- 1 create schema
 create schema dtwh_admin;
 
--- 2 create table
+-- 2 create tables
+-- table datawh
 create table dtwh_admin.datawh (
-    dtwh_id integer not null ,
+    dtwh_id integer not null unique primary key,
     db_env_id integer not null,
     client_id integer not null,
     db_name text,
@@ -16,11 +17,23 @@ create table dtwh_admin.datawh (
     db_type_code text not null
 );
 
--- 3 insert 2 records
+-- table client
+create table dtwh_admin.client(
+    client_id integer not null primary key,
+    client_name varchar(50),
+    is_active bool not null
+);
+
+-- 3 insert 
+
+-- 2 records into table datawh
 
 insert into dtwh_admin.datawh(
 	dtwh_id, db_env_id, client_id, db_name,db_host, db_version, description, id, is_active, db_type_code) values 
 	(10,1,1010,'pitasare','pita',1,'regional hospital',1,true,'dev')
 	(20,1,1011,'labesare','labe',1,'regional clinic',1,true,'dev');
+
+
+
 
 
