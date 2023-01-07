d9130e313d3ee0893aaa61ddfc7b0e01a96f15f4
thiernobarry <tmabarry@yahoo.com>
2023-01-06 21:39:45 -0500

    dataset taxi added

---
+++ postgres_projects/datasets/taxi.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# download the dataset from taxi and limousine NY https://www.nyc.gov/site/tlc/about/data.page
+wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-01.parquet
