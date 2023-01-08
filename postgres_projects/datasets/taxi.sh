e027e5cfcbab1486f4e8d4f6a3aea365d12e04a8
thiernobarry <tmabarry@yahoo.com>
2023-01-08 11:46:33 -0500

    updated dataset

--- postgres_projects/datasets/taxi.sh
+++ postgres_projects/datasets/taxi.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-# download the dataset from taxi and limousine NY https://www.nyc.gov/site/tlc/about/data.page
-wget https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-01.parquet
+
+# download the yellow taxi trip records dataset of october 2022 and rename it as NY2022_10.parquet 
+# from taxi and limousine NY https://www.nyc.gov/site/tlc/about/data.page
+wget -O NY2022_10.parquet https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2022-01.parquet
+
+# downlaod the taxi zone Maps and Lookup Tables
+wget https://d37ci6vzurychx.cloudfront.net/misc/taxi+_zone_lookup.csv
+
+# for data dictionary refer to 
+# https://www.nyc.gov/assets/tlc/downloads/pdf/data_dictionary_trip_records_yellow.pdf
